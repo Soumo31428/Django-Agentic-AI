6e88a7c642e1c1090695e901f7705a60b1b0278e
Soumo Chakraborty <soumo.study8172@gmail.com>
2026-08-02 16:00:54 +0530

    Add Tables to admin site.

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Product, Order, RefundRequest
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(RefundRequest)
