a497ff5c5257b7459762b5f4c984e3660c53ee4b
Soumo Chakraborty <soumo.study8172@gmail.com>
2026-08-04 22:48:42 +0530

    Add login template and fix template directory config

--- ai_employee_main/settings.py
+++ ai_employee_main/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ai_employee_main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- ai_employee_main/urls.py
+++ ai_employee_main/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # login view
+
+    path('login/', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
 ]
