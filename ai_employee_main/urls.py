43bce725ea8cdfc98cb3588ad5445d4ef435423a
Soumo Chakraborty <soumo.study8172@gmail.com>
2026-08-05 09:41:15 +0530

    added login, logout, orders page

--- ai_employee_main/urls.py
+++ ai_employee_main/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.shortcuts import redirect
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -25,4 +25,7 @@ urlpatterns = [
     # login view
 
     path('login/', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
+
+    path('orders/', include('orders.urls')),
 ]
